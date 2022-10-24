96805501c4f5ebb89ad4129be127324ced9fee1a
B-Pence <73327227+B-Pence@users.noreply.github.com>
2022-10-23 23:40:32 -0500

    upload of some more pascal code

---
+++ Free-Pascal-Code/Test.pas
@@ -0,0 +1,24 @@
+(*
+ * Project: Free-Pascal-Code
+ * User: brpen
+ * Date: 10/16/2022
+ *)
+program Test (Input, Output);
+
+const
+  name = 'B-Pence';
+  Year = 2022;
+
+begin
+  
+  (*
+   *multi-line 
+   *comment
+   *)
+  
+  {
+    free form brace comment
+  }
+  
+  //single line commen
+end.
---
+++ Pascal-Cont/Test.pas
@@ -0,0 +1,9 @@
+(*
+ * Project: Pascal-Cont
+ * User: brpen
+ * Date: 10/23/2022
+ *)
+program Test;
+begin
+  
+end.
